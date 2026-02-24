1e55219a85e203062fdca2d7eee25be0ec20365c
Marluvt1 <marluvantonder@gmail.com>
2026-02-24 13:37:34 +0200

    Initial CyberRose Ecosystem: Break the World

---
+++ CyberRose/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CyberRose.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ CyberRose/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CyberRose.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
